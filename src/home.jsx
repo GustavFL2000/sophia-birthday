257f647d41f1e2a7a8b458f578faba1c46821841
LumreLars <gustavfl13@gmail.com>
2025-09-16 10:27:12 +0200

    first commit

---
+++ src/home.jsx
@@ -0,0 +1,10 @@
+
+import './home.css';
+export default function Home() {
+    return (
+        <div>
+            <h1>Tillykke med Fødselsdagen sødeste Sophia</h1>
+            <p></p>
+        </div>
+    );
+}
